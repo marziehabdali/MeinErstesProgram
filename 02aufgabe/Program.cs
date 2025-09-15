cc814194650ba0ef00ace3b14e526ba78236be22
marziehabdali <marzieabdali@gmail.com>
2025-09-15 13:35:25 +0000

    notiz

---
+++ 02aufgabe/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        // 
+        string meinName = "Marzieh";
+        int meinAlter = 32;
+        double meinGehalt = 2500.90;
+        bool istStudent = true;   
+        char meinInitial = 'M';
+
+        
+        Console.WriteLine("Name: " + meinName);
+        Console.WriteLine("Alter: " + meinAlter + " Jahre");
+        Console.WriteLine("Gehalt: " + meinGehalt + " Euro");
+        Console.WriteLine("Student: " + istStudent);
+        Console.WriteLine("Initial: " + meinInitial);
+
+        Console.ReadKey();
+    }
+}
---
+++ program.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace MeinErstesProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hallo Welt!");
+            Console.WriteLine("Willkommen bei C# Programmierung!");
+            Console.WriteLine("Drücken Sie eine beliebige Taste...");
+            Console.ReadKey();
+        }
+    }
+}
